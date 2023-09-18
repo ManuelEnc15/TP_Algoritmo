b962e10a7cdaf3d21840accc3542697ca232c3e0
ManuelEnc15 <122401516+ManuelEnc15@users.noreply.github.com>
2023-09-17 21:48:41 -0300

    Update Opc1.cs

--- Opc1.cs
+++ Opc1.cs
@@ -1,1 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
+namespace TP_Abogados
+{
+	/// <summary>
+	/// Description of Opc1.
+	/// </summary>
+	public class Opc1: Exception
+	{
+	}
+}
